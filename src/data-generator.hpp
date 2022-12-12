2f38730b531615013ff8a8f074ce799c7051eddd
Marhes <marcinhesse@gmail.com>
2022-12-12 14:46:21 +0100

    added tests for gender generator and pesel generator

--- src/data-generator.hpp
+++ src/data-generator.hpp
@@ -7,15 +7,13 @@
 #include <memory>
 #include <random>
 
-class DataGenerator{
+struct DataGenerator{
     int countLinesInFile(const std::string & fileName);
-public:
     std::string getSource(const short & num);
     std::string generateName (const std::string & data);
     std::string generatePesel(const std::string & sex);
     std::string generateSex();
     std::string generateIndexNumber();
-    //std::string getAddress();
     std::string generateSalary();
     std::shared_ptr<Address> generateAddress();
 };
--- tests/DataGeneratorTest/data-generator-ut.cpp
+++ tests/DataGeneratorTest/data-generator-ut.cpp
@@ -1,6 +1,49 @@
+#include <algorithm>
 #include <gtest/gtest.h>
 #include "data-generator.hpp"
 
-TEST(A,a){
-	EXPECT_EQ(4 * 4,16);
+TEST(genSex,doesGenSexReturnStringForM){
+	DataGenerator dg{};
+	std::string s = dg.generateSex();
+	EXPECT_EQ(s.size(),1);
+}
+//----------------------------------------------
+struct peselGenerator: public ::testing::Test{
+	DataGenerator dg{};
+	std::string s = dg.generatePesel("F");
+};
+
+TEST_F(peselGenerator,doesPESELcontain11digits){
+	EXPECT_EQ(s.size(),11);
+}
+
+TEST_F(peselGenerator,doesPESELcontainOnlyDigits){
+	auto a = std::all_of(begin(s),end(s),isdigit);
+	EXPECT_TRUE(a);
+}
+
+TEST_F(peselGenerator,doesPESELcontainEvenNumberInCaseFemale){
+	int genderNum = (s[9] - 48) % 2;
+	if(genderNum){
+		EXPECT_NE(genderNum,0);
+	}
+	else{
+		EXPECT_EQ(genderNum,0);
+	}
+}
+
+TEST_F(peselGenerator,doesPESELcontainRightFirstDigitOfMonth){
+	int firstDigitOfMonth = (s[2] - 48);
+	EXPECT_LT(firstDigitOfMonth,4);
+}
+
+TEST_F(peselGenerator,doesPESELcontainRightSecondDigitOfMonth){
+	int firstDigitOfMonth = (s[2] - 48);
+	int secondDigitOfMonth = (s[3] - 48);
+	if(firstDigitOfMonth == 1 or firstDigitOfMonth == 3){
+		EXPECT_LT(secondDigitOfMonth,3);
+	}
+	else{
+		EXPECT_GT(secondDigitOfMonth,0);
+	}
 }
